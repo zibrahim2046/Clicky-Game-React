6da372b14bfd010c66fc84860ae9fe55b6408f47
Ian <ian@macbook-pro.lan>
2020-06-12 14:13:24 -0500

    Updates

---
+++ precache-manifest.bccc6d0e5daf9a8fb518e85ad48d57b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45f25b4436a5160703767085bd7a7ab",
+    "url": "/Clicky-Game-React/index.html"
+  },
+  {
+    "revision": "5555d2dbe63f37f7acf9",
+    "url": "/Clicky-Game-React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fa00893c7741e4918d4",
+    "url": "/Clicky-Game-React/static/js/2.990d369b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game-React/static/js/2.990d369b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5555d2dbe63f37f7acf9",
+    "url": "/Clicky-Game-React/static/js/main.5f571e44.chunk.js"
+  },
+  {
+    "revision": "61f7995e72cb9b8b2fa9",
+    "url": "/Clicky-Game-React/static/js/runtime-main.79c3288d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clicky-Game-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
